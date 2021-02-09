582b5d6a381c1430f1b5041b3410f2d9e0890e3d
mikepmerritt <mikepmerritt@gmail.com>
2021-02-08 21:29:41 -0500

    initial commit

---
+++ src/CollegeStudentDriver.java
@@ -0,0 +1,9 @@
+
+public class CollegeStudentDriver {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is the start of the project, so more will come soon.");
+	}
+
+}
