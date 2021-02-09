6af9364246b054f047275a7cb2c74d5ad52a8ce8
mikepmerritt <mikepmerritt@gmail.com>
2021-02-08 21:45:33 -0500

    added starter code for CollegeStudent class file

---
+++ src/CollegeStudent.java
@@ -0,0 +1,12 @@
+
+public class CollegeStudent {
+	
+	private int age;
+	private String college;
+	private int numAssignments;
+	
+	public CollegeStudent(int studentAge, String collegeName) {
+		age = studentAge;
+		college = collegeName;
+	}
+}
--- src/CollegeStudentDriver.java
+++ src/CollegeStudentDriver.java
@@ -2,8 +2,11 @@
 public class CollegeStudentDriver {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
 		System.out.println("This is the start of the project, so more will come soon.");
+		
+		CollegeStudent michael = new CollegeStudent(19, "Quinnipiac");
+		
 	}
 
 }
