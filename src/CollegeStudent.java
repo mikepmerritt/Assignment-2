d6152210b8834fcc6425a0399038880659573d5a
mikepmerritt <mikepmerritt@gmail.com>
2021-02-11 18:09:15 -0500

    added student name and a method for doing assignments

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,12 +1,26 @@
 
 public class CollegeStudent {
 	
+	private String name;
 	private int age;
 	private String college;
 	private int numAssignments;
 	
-	public CollegeStudent(int studentAge, String collegeName) {
+	public CollegeStudent(String studentName, int studentAge, String collegeName) {
+		name = studentName;
 		age = studentAge;
 		college = collegeName;
+		numAssignments = (int) (Math.random()*3+1);
+	}
+	
+	public void doAssignment() {
+		if(numAssignments > 0) {
+			int timeSpent = (int) (Math.random()*30+15);
+			System.out.println(name + " finished the assignment in " + timeSpent + " minutes.");
+			numAssignments--;
+		}
+		else {
+			System.out.println(name + " has no more assignments to do!");
+		}
 	}
 }
--- src/CollegeStudentDriver.java
+++ src/CollegeStudentDriver.java
@@ -5,8 +5,11 @@ public class CollegeStudentDriver {
 		
 		System.out.println("This is the start of the project, so more will come soon.");
 		
-		CollegeStudent michael = new CollegeStudent(19, "Quinnipiac");
+		CollegeStudent michael = new CollegeStudent("Michael", 19, "Quinnipiac");
 		
+		for(int i = 0; i < 4; i++) {
+			michael.doAssignment();
+		}
 	}
 
 }
